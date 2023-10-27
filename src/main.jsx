e60b34afa6738e0b09c80c404f40470fee124d22
Minki Baek <alsrl6678@naver.com>
2023-10-27 20:52:18 +0900

    스타일 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './styles/global'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider>
     <App />
+      < GlobalStyles />
+
+    </ThemeProvider>
   </React.StrictMode>,
 )
